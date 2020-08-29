3d8f02c48a96e50458bb2cd631010d0703d420b9
bobo mallick <mallickbobo@gmail.com>
2020-08-30 00:08:41 +0530

    thanks rosogolla

---
+++ src/components/Notfound/Notfound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Notfound = () => {
+    return (
+        <div>
+            <h2>Not Found!</h2>
+            <h3>404</h3>
+        </div>
+    );
+};
+
+export default Notfound;
